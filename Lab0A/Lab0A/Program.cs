f3b2c9947ff826a7fb5ed83ffaf40312370d6a4f
Fitywe <96216623+Pure227@users.noreply.github.com>
2023-03-27 23:04:50 +0700

    Lab0a

---
+++ Lab0A/Lab0A/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Name : Arun Srisawad");
+Console.WriteLine("ID : 643450095-5");
+Console.WriteLine("E-mail : arun.s@kkumail.com");
+Console.ReadLine();
